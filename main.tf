7b07952a82cdd079b454f4781d7c0d706ac957b0
vishalsingh-iitt <vishalsingh.iitt@gmail.com>
2025-06-19 11:30:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,11 +18,29 @@ data "aws_vpc" "default" {
   default = true
 }
 
+
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
 
   vpc_security_group_ids = [aws_security_group.blog.id]
+  subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "Learning Terraform"
@@ -34,7 +52,7 @@ resource "aws_security_group" "blog" {
   tags = {
     Terraform = "true"
   }
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.blog_vpc.vpc_id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
